d40b42b6622b1302116f602d260c605b9a594506
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2022-02-21 16:05:09 -0800

    Adds i18n-next library and updates API

--- packages/app/src/components/Skills.tsx
+++ packages/app/src/components/Skills.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { APIS, fetchResourcePromise } from 'shared'
+import { useTranslation } from 'react-i18next';
 
 const resumeResource = fetchResourcePromise({
   url: APIS.RESUME_API,
   responseLookupKey: 'resume' 
 });
 
-const resume = function () {
+const Skills = function () {
+  const { t, i18n } = useTranslation();
   return (
     <div className="flex">
       <div className="w-screen">
         <div className="px-20">
-        <p className="font-bold text-xl">Skills</p>
+        <p className="font-bold text-xl">{t('section_skills')}</p>
           <ul>{
             resumeResource.pageData.read().map((value: any, index: any) => {
-              return <li className="mb-5" key={index}> {value.title}: {value.skills}</li>
+              return <li className="mb-5" key={index}> <span className="font-bold">{value.title} :</span> {value.skills}</li>
             })
             }</ul>  
         </div>
@@ -23,7 +25,7 @@ const resume = function () {
   );
 }
 
-export default resume;
+export default Skills;
 
 
 
--- packages/app/src/components/Work.tsx
+++ packages/app/src/components/Work.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { APIS, fetchResourcePromise } from 'shared'
+import { useTranslation } from 'react-i18next';
 
 const workResource = fetchResourcePromise({
   url: APIS.WORK_API,
   responseLookupKey: 'work' 
 });
-const about = function () {
+const Work = function () {
+  const { t, i18n } = useTranslation();
   return (
     <div className="flex">
       <div className="w-screen">
         <div className="px-20 py-10">
-        <p className="font-bold text-xl">Work Experience</p>
+        <p className="font-bold text-xl">{t('section_work')}</p>
         <ul>{
             workResource.pageData.read().map((value: any, index: any) => {
             return <li key={index} className="mb-5"> 
-              <p className="text-lg">{value.title}: <a href={value.url} target="_blank" rel="noopener noreferrer">{value.company}</a> {value.period}</p>
+              <p className="text-lg font-bold">{value.title} @ <a href={value.url} target="_blank" rel="noopener noreferrer">{value.company}</a></p>
+              <span className="text-md font-bold"> {value.period} </span>
               <p>{value.summary}</p>
             </li>
             })
@@ -25,7 +28,7 @@ const about = function () {
   );
 }
 
-export default about;
+export default Work;
 
 
 
